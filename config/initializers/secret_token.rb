521a29d509c712a68ea656aa15bbffc6ca0063e1
Denisov Lev <denisovlev@ya.ru>
2013-10-04 14:14:24 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewsPortal::Application.config.secret_token = '1f766021ad330c288f79d6f8bc24377e7cca54fe49f3954bac693cb0d73e30e32f3859fde1bcaa0d05fe5d558cd1a33267ec674f64746109394c6711ca88203d'
