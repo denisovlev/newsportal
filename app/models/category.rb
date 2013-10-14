426f7ce1359db6dd954133229bbebf6ca946e080
Denisov Lev <denisovlev@ya.ru>
2013-10-14 18:21:25 +0400

    Removed comlex SQL from Categories.user_subscribed

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,13 +4,4 @@ class Category < ActiveRecord::Base
   has_many :users, through: :subscriptions
   attr_accessible :name, :category_id
   validates :name, presence: true
-
-  def self.user_subscribed(user_id)
-  	return Category.select("categories.id, categories.name, (NOT(subscriptions.id IS NULL)) AS subscribed, subscriptions.id AS subscription_id")
-  			.joins("LEFT JOIN subscriptions ON subscriptions.category_id = categories.id AND subscriptions.user_id = #{user_id}")
-  end
-
-  def subscribed
-    ActiveRecord::ConnectionAdapters::Column.value_to_boolean(self[:subscribed])
-  end
 end
